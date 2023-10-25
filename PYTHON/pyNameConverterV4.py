7c8e7de0d5786b741a33b6a40ae3e61aec250ba6
frenchpose <frenchpose.contact@gmail.com>
2023-10-25 15:48:32 +0200

    add comments

--- PYTHON/pyNameConverterV4.py
+++ PYTHON/pyNameConverterV4.py
@@ -17,17 +17,17 @@ def rename_files(directory_path):
         if os.path.isfile(os.path.join(directory_path, filename)):
             new_filename = filename.lower().replace(" ", "_").replace("'", "_").replace('"', "_").replace("-", "_").replace(",","_")
             new_filepath = os.path.join(directory_path, new_filename)
-            if os.path.exists(new_filepath) and new_filepath != os.path.join(directory_path, filename):
+            if os.path.exists(new_filepath) and new_filepath != os.path.join(directory_path, filename): #verifie si le fichier existe et s'il n'y a pas un autre fichier avec le même nom
                 try:
-                    print(f"filename = {filename} ")
-                    print(f"new_filename = {new_filename} ")
-                    os.remove(new_filepath)
-                    os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
+                    #print(f"filename = {filename} ")
+                    #print(f"new_filename = {new_filename} ")
+                    os.remove(new_filepath) #supprime le fichier déjà présent
+                    os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename)) #renomme le fichier comme prevu
                 except OSError as e:
-                        print(f"Erreur lors de la suppression du fichier : {e}")
+                        print(f"Erreur lors de la suppression du fichier : {e}") #sort une erreur
                         continue
             else:
-                os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
+                os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))  #renomme le fichier comme prevu
             
 if __name__ == "__main__":
     paths = input("Entrez les chemins des dossiers (séparés par des virgules) : ").split(',')
