d1e098decb882a4360a429aa8e171da2b1f440da
frenchpose <frenchpose.contact@gmail.com>
2023-11-02 14:05:42 +0100

    creation cours et exos openclassroom

---
+++ JAVASCRIPT/OPENCLASSROOM/scripts/variables.js
@@ -0,0 +1,18 @@
+// Déclarez une variable listeMots qui est un tableau
+let listeMots = ["Cachalot","Pétunia","Serviette"]
+
+// Déclarez une deuxième variable score, initialisée à 0, qui contiendra le nombre de mots correctement tapés par l’utilisateur
+let score = 0
+
+//Déclarer une variable i qui est utilisé dans la boucle for
+let i = 0
+
+// Déclarer une variable motUtilisateur qui sert à récupérer le mot de l'utilisateur
+let motUtilisateur
+
+//Déclarer une variable pour le testMotUtilisateur === TRUE or FALSE
+let ComparaisonMotUtilisateur
+
+//Déclarer deux variables pour les résultats des deux fonctions
+let ResultUpdateMotUtilisateur
+let ResultTestMotUtilisateur
