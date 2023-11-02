78444dffa16666dab54fa6e970808c77b577c58c
yindiana <147611074+yindiana@users.noreply.github.com>
2023-11-02 14:50:08 +0100

    Update functions.js

--- JAVASCRIPT/OPENCLASSROOM/scripts/functions.js
+++ JAVASCRIPT/OPENCLASSROOM/scripts/functions.js
@@ -1,64 +1,27 @@
-// // pseudo code de l'algorithme
-// // Si le mot proposé est correct
-// // Alors points += 1
-
-// // Déclarez une variable listeMots qui est un tableau
-// let listeMots = ["Cachalot","Pétunia","Serviette"]
-
-// // Déclarez une deuxième variable score, initialisée à 0, qui contiendra le nombre de mots correctement tapés par l’utilisateur
-// let score = 0
-
-// // Déclarer une variable motUtilisateur qui sert à récupérer le mot de l'utilisateur
-// let motUtilisateur
-
-// for (let i = 0; i < 3; i++) {
-//     //Modification de la variable motUtilisateur
-//     motUtilisateur = prompt("Entrez le mot " + listeMots[i])
-//     // structure conditionnelle 1 pour savoir si motUtilisateur est identique à listeMots[i] et incrémentation de la variable score
-//     if (motUtilisateur === listeMots[i]) {
-//         score += 1
-//         console.log("bien joué ! Votre score est de " + score)
-//     } else {
-//         console.log("Vous avez fait une faute de frappe, vous avez écrit " + motUtilisateur + ". Votre score est de " + score)
-//     }
-// }
-
-
-
-
-
-// pseudo code de l'algorithme
-// Si le mot proposé est correct
-// Alors points += 1
-
-function updateMotUtilisateur(listeMots, i) {
+function recupMotUtilisateur() {
     //Modification de la variable motUtilisateur
     motUtilisateur = prompt("Entrez le mot " + listeMots[i]);
     return motUtilisateur;
 }
 
-function testMotUtilisateur(motUtilisateur, listeMots, score) {
+function comparaisonMotUtilisateur() {
     // structure conditionnelle 1 pour savoir si motUtilisateur est identique à listeMots[i] et incrémentation de la variable score
     if (motUtilisateur === listeMots[i]) {
         score += 1
-        ComparaisonMotUtilisateur = "bien joué ! Votre score est de " + score
-    } else {
-        ComparaisonMotUtilisateur = "Vous avez fait une faute de frappe, vous avez écrit " + motUtilisateur + ". Votre score est de " + score
     }
-    return ComparaisonMotUtilisateur;
+    return score;
 }
 
+function afficherResultat() {
+    let resultat = score
+    console.log("Votre résultat est de : " + resultat + " sur " + listeMots.length)
+}
 
-
-while (i <= listeMots.length) {
-    //Fonction mot utilsateur
-    ResultUpdateMotUtilisateur = updateMotUtilisateur()
-    console.log(ResultUpdateMotUtilisateur)
-
-    //Fonction Test du mot utilsateur
-    ResultTestMotUtilisateur = testMotUtilisateur()
-    console.log(ResultTestMotUtilisateur)
-
-    //Incrémentation de i
-    i++
+function boucleParseDictionnaire() {
+    while (i < listeMots.length){
+        recupMotUtilisateur()
+        comparaisonMotUtilisateur()
+        afficherResultat()
+        i++
+    }
 }
