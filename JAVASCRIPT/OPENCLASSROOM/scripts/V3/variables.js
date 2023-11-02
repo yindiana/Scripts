ab552f05fe9853fc826c694d8ee7a5baf618a69e
yindiana <147611074+yindiana@users.noreply.github.com>
2023-11-02 15:47:39 +0100

    Create variables.js

---
+++ JAVASCRIPT/OPENCLASSROOM/scripts/V3/variables.js
@@ -0,0 +1,18 @@
+//Variable de choix entre liste de mot ou de phrases
+let choixMotsOuPhrases
+
+//Variable de conversion pour choixMotsOuPhrases
+let motProposes
+
+// Déclarer une variable motUtilisateur qui sert à récupérer le mot de l'utilisateur
+let motUtilisateur;
+
+// Déclarez une variable listeMots qui est un tableau
+let listeMots = ["Cachalot","Pétunia","Serviette"]
+
+// Déclarez une variable listePhrases qui est un tableau
+let listePhrases = ["Je suis aveugle", "Mon nom est bob", "Invites moi au ciné"]
+
+let i = 0
+
+let score = 0
