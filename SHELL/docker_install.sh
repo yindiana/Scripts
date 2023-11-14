91e4d783c4aa47cc9bad09795453bba41a2cddd0
yindiana <147611074+yindiana@users.noreply.github.com>
2023-11-14 21:23:40 +0100

    Update docker_install.sh

--- SHELL/docker_install.sh
+++ SHELL/docker_install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #Variables
-docker_check=$(systemctl status dockers)
+docker_check=$(systemctl status docker)
 script_dl=$(curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh)
 user_creation=$(useradd -c “docker admin user” -s /bin/bash dockeradm)
 no_sudo_user=$(sudo usermod -aG docker dockeradm)
+test_output=$(echo $?)
 
 #Installations
 $docker_check
-if [[echo $? !== 0]]
+if [[ $test_output != 0 ]]
 then
     $script_dl
     grep -i dockeradm /etc/passwd
-    if [[echo $? !== 0]]
+    if [[ $test_output != 0 ]]
     then
         $user_creation && $no_sudo_user
     else
