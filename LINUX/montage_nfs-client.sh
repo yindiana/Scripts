268c2a200d7f98123dd5ee1674d3633afa922069
yindiana <147611074+yindiana@users.noreply.github.com>
2024-01-05 14:06:38 +0100

    Update montage_nfs-client.sh

--- LINUX/montage_nfs-client.sh
+++ LINUX/montage_nfs-client.sh
@@ -11,4 +11,7 @@ echo "entrez l'IP du point de montage NFS"
 read ip_serveur_nfs
 echo "entrez le path du dossier partagé sur le point de montage NFS"
 read $nom_partage_nfs
-sudo mount -t nfs $ip_serveur_nfs:/nom_partage_nfs $pointdemontage
+sudo mount -t nfs $ip_serveur_nfs:$nom_partage_nfs $pointdemontage
+
+#persistance avec le fstab
+echo "$ip_serveur_nfs:$nom_partage_nfs $pointdemontage     nfs     auto,_netdev,,nofail        0 0" >> /etc/fstab
