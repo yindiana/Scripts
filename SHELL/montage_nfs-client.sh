b33d4c794553957ecb73a59482a9d3d7dbf29627
yindiana <147611074+yindiana@users.noreply.github.com>
2024-01-06 09:37:42 +0100

    Update montage_nfs-client.sh

--- SHELL/montage_nfs-client.sh
+++ SHELL/montage_nfs-client.sh
@@ -1,8 +1,8 @@
 #installlation du client
-sudo apt install nfs-common
+sudo apt install nfs-common -y
 
 #creation du point de montage
-echo "entrez le path où sera monté le point NFS"
+echo "entrez le path où sera monté le point NFS sur le client"
 echo "Exemple : /ceci/est/un/point/de/montage"
 read pointdemontage
 sudo mkdir -p $pointdemontage
@@ -11,14 +11,16 @@ sudo mkdir -p $pointdemontage
 echo "entrez l'IP du point de montage NFS"
 echo "Exemple : 192.168.1.1"
 read ip_serveur_nfs
-echo "entrez le path du dossier partagé sur le point de montage NFS"
+echo "entrez le path du dossier partagé sur le serveur cible"
 echo "Exemple : /export/mon-point-nfs"
-read $nom_partage_nfs
+read nom_partage_nfs
 sudo mount -t nfs $ip_serveur_nfs:$nom_partage_nfs $pointdemontage
 #verification
 echo "vérifiez que le montage est fait"
 df -h | grep $pointdemontage
+showmount -a
 showmount -e
+showmount -d
 
 #persistance avec le fstab
 echo "$ip_serveur_nfs:$nom_partage_nfs $pointdemontage     nfs     auto,_netdev,,nofail        0 0" >> /etc/fstab
