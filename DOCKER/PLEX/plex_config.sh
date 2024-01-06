4e44a6864383d61d2c1fb61b998b3ff4002f33b9
yindiana <147611074+yindiana@users.noreply.github.com>
2024-01-06 09:27:22 +0100

    Update plex_config.sh

--- DOCKER/PLEX/plex_config.sh
+++ DOCKER/PLEX/plex_config.sh
@@ -1,4 +1,4 @@
 #Prerequis
 #ajouter les fichiers Scritps/LINUX/montage_nfs_client.sh et Scritps/LINUX/user_&_group_add.sh ici
 
-./user_&_group_add.sh && ./montage_nfs_client.sh
+./gestion_user_group.sh && ./montage_nfs_client.sh
