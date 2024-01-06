7b2729d97109005c7a11f37a60342368171e7597
yindiana <147611074+yindiana@users.noreply.github.com>
2024-01-06 11:32:19 +0100

    Update plex_config.sh

--- DOCKER/PLEX/plex_config.sh
+++ DOCKER/PLEX/plex_config.sh
@@ -2,3 +2,6 @@
 #ajouter les fichiers Scritps/LINUX/montage_nfs_client.sh et Scritps/LINUX/user_&_group_add.sh ici
 
 ./gestion_user_group.sh && ./montage_nfs_client.sh
+#/opt/plex/config /opt/plex/films /opt/plex/series /opt/plex/musics /opt/plex/dcmi
+#port par defaut : 32400
+#if error : This XML file does not appear to have any style information associated with it. The document tree is shown below --> http://ipaddress:32400/manage
